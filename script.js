6e2f8b3a74f110f3699812217a7214393410af32
coussss <adamjbox@gmail.com>
2023-01-04 18:30:36 +0000

    Created function to return a random choice of rock, paper or scissors

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let computerChoice = undefined
+    let x = Math.random();
+    if (x < 0.33) {
+        computerChoice = "rock";
+    } else if (x > 0.33 && x > 0.66) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    console.log(computerChoice)
+}
+
