c3cc1cc670bf52cfe93b4da4866128f776f293db
coussss <adamjbox@gmail.com>
2023-01-08 19:08:07 +0000

    Fixed minor error (used > instead of < causing incorrect result)

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ function getComputerChoice() {
     let x = Math.random();
     if (x < 0.33) {
         computerChoice = "rock";
-    } else if (x > 0.33 && x > 0.66) {
+    } else if (x > 0.33 && x < 0.66) {
         computerChoice = "paper";
     } else {
         computerChoice = "scissors";
     }
     console.log(computerChoice)
+    console.log(x)
 }
 
+getComputerChoice()
