0a1b54fc41a2d50e0e0ed6432081d08c4c3e0b6e
coussss <adamjbox@gmail.com>
2023-01-09 17:42:31 +0000

    Created a message for invalid input and new variables to keep score

--- script.js
+++ script.js
@@ -13,16 +13,28 @@ function getComputerChoice() {
 function playRound(userChoice, computerSelection) {
     if (userChoice == "rock" && computerSelection == "scissors") {
         console.log("You win! Rock beats Scissors!");
+        playerWin = playerWin + 1;
+        console.log(`Score: ${playerWin} ${"-"} ${computerWin}`);
     } else if (userChoice == "paper" && computerSelection == "scissors") {
         console.log("You lose! Scissors beats Paper!");
+        computerWin = computerWin + 1;
+        console.log(`Score: ${playerWin} ${"-"} ${computerWin}`);
     } else if (userChoice == "rock" && computerSelection == "paper") {
         console.log("You lose! Paper beats Rock!");
+        computerWin = computerWin + 1;
+        console.log(`Score: ${playerWin} ${"-"} ${computerWin}`);
     } else if (userChoice == "scissors" && computerSelection == "paper") {
         console.log("You Win! Scissors beats Paper!");
+        playerWin = playerWin + 1;
+        console.log(`Score: ${playerWin} ${"-"} ${computerWin}`);
     } else if (userChoice == "paper" && computerSelection == "rock") {
         console.log("You Win! Paper beats Rock!");
+        playerWin = playerWin + 1;
+        console.log(`Score: ${playerWin} ${"-"} ${computerWin}`);
     } else if (userChoice == "scissors" && computerSelection == "rock") {
         console.log("You lose! Rock beats Scissors!");
+        computerWin = computerWin + 1;
+        console.log(`Score: ${playerWin} ${"-"} ${computerWin}`);
     } else if (userChoice == computerSelection) {
         console.log("It's a draw!")
     }
@@ -34,11 +46,19 @@ function game() {
 
         let computerSelection = getComputerChoice();
 
+        if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors") {
+            console.log("Invalid attack!");
+            i--;
+        }
+
         console.log("Computer chose "+computerSelection)
         
         playRound(userChoice, computerSelection)
     }
 }
+
+playerWin = 0
+computerWin = 0
 
 console.log(game());
 
